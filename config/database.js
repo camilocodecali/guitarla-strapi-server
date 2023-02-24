156acc18732f8a3e2d585cd5d671076eb3af1486
CamiloR <camilorodriguezco7@gmail.com>
2023-02-24 00:59:18 -0500

    deployment

--- config/database.js
+++ config/database.js
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', 'dpg-ccprjoqen0hr84ner7ig-a.oregon-postgres.render.com'),
+      host: env('DATABASE_HOST', 'dpg-cfbljb82i3mjdumup6ag-a.oregon-postgres.render.com'),
       port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'guitarla_mt8d'),
+      database: env('DATABASE_NAME', 'guitarla_43wo'),
       user: env('DATABASE_USERNAME', 'root'),
-      password: env('DATABASE_PASSWORD', 'NP4AG6wOTS4x1QCU0gLBu1isWRo5HVMq'),
+      password: env('DATABASE_PASSWORD', 'KaEnBlfol2m4j6BF2kF6uKDKbHnWwGST'),
       ssl: env.bool('DATABASE_SSL', true),
     },
   },
